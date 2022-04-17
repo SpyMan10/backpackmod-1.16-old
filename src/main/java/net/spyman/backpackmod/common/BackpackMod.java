68efb18085205455c62b1c5fb0f31baf2830ab78
SpyMan10 <dev.spyman@gmail.com>
2022-04-17 22:05:47 +0200

    added new config to customize backpack items

--- src/main/java/net/spyman/backpackmod/common/BackpackMod.java
+++ src/main/java/net/spyman/backpackmod/common/BackpackMod.java
@@ -3,26 +3,38 @@ package net.spyman.backpackmod.common;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.spyman.backpackmod.common.config.BackpackCfgFile;
 import net.spyman.backpackmod.common.init.BackpackItems;
 import net.spyman.backpackmod.common.init.BackpackRecipes;
 import net.spyman.backpackmod.common.item.BackpackItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class BackpackMod implements ModInitializer {
 
-    public static final String MODID = "backpackmod";
+    public static final String NAME = "BackpackMod";
+    public static final String MODID = NAME.toLowerCase();
 
-    public static final ItemGroup GROUP = FabricItemGroupBuilder.create(identify("group")).icon(() -> new ItemStack(BackpackItems.LEATHER_BACKPACK)).build();
+    public static final ItemGroup GROUP = FabricItemGroupBuilder.create(identify("group")).icon(() -> new ItemStack(Items.APPLE)).build();
 
     public static final Identifier PACKET_RENAME_BACKPACK = identify("packet_rename_backpack");
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+
     @Override
     public void onInitialize() {
+        // initialize / load configuration file
+        BackpackCfgFile.instance().init();
+        BackpackCfgFile.instance().load();
+
         BackpackItems.register();
         BackpackRecipes.register();
 
--- src/main/java/net/spyman/backpackmod/common/item/BackpackItem.java
+++ src/main/java/net/spyman/backpackmod/common/item/BackpackItem.java
@@ -62,6 +62,10 @@ public class BackpackItem extends Item {
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
         tooltip.add(translate("tooltip.size", this.width, this.height, this.width * this.height).formatted(Formatting.GRAY));
         tooltip.add(translate("tooltip.how.to.rename").formatted(Formatting.ITALIC, Formatting.GRAY));
+
+        if (this.isFireproof()) {
+            tooltip.add(translate("tooltip.fireproof").formatted(Formatting.GOLD));
+        }
     }
 
     public static final void openScreen(PlayerEntity user, ItemStack stack) {
